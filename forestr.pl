bdd868ac13e08fac14281a4a4bbd7332f0f6cb08
AskerAttackteam <48393813+AskerAttackteam@users.noreply.github.com>
2019-05-16 19:54:45 +0300

    Add files via upload

---
+++ forestr.pl
@@ -0,0 +1,20 @@
+[STDOUT] Terminal Agent binary is downloaded remotely
+[STDOUT] 2019/05/16 14:53:23 Terminal-agent configuration
+[STDOUT] 2019/05/16 14:53:23   Server
+[STDOUT] 2019/05/16 14:53:23     - Address: :4411
+[STDOUT] 2019/05/16 14:53:23     - Base path: '/[^/]+'
+[STDOUT] 2019/05/16 14:53:23   Terminal
+[STDOUT] 2019/05/16 14:53:23     - Slave command: ''
+[STDOUT] 2019/05/16 14:53:23     - Activity tracking enabled: true
+[STDOUT] 2019/05/16 14:53:23   Authentication
+[STDOUT] 2019/05/16 14:53:23     - Enabled: true
+[STDOUT] 2019/05/16 14:53:23     - Tokens expiration timeout: 10m
+[STDOUT] 2019/05/16 14:53:23   Workspace master server
+[STDOUT] 2019/05/16 14:53:23     - API endpoint: https://codenvy.io/api
+[STDOUT] 2019/05/16 14:53:23 
+[STDOUT] 2019/05/16 14:53:23 ⇩ Registered HTTPRoutes:
+[STDOUT] 
+[STDOUT] 2019/05/16 14:53:23 Terminal routes:
+[STDOUT] 2019/05/16 14:53:23 ✓ Connect to pty(websocket) ............... GET    /pty
+[STDOUT] 2019/05/16 14:53:23 
+[STDOUT] Workspace Agent will be downloaded from Workspace Master
